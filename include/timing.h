ee8588fc9eb0de1a2c5e2cb28f69e5af9b43ab89
Ken  Ledford <kenledford@Kens-MBP.attlocal.net>
2020-02-15 16:35:22 -0800

    adding code

--- include/timing.h
+++ include/timing.h
@@ -1,14 +1,14 @@
 #ifndef TIMING_H
 #define TIMING_H
 
-#include<iostream>
+#include <iostream>
 #include <chrono>
-#include <thread>  
+#include <thread>
 
 namespace extend
 {
 
-const int DEFUALT_SLEEP_THRESH_MS= 15; 
+const int DEFUALT_SLEEP_THRESH_MS = 15;
 
 u_int64_t getTimeMilliSecs()
 {
@@ -16,8 +16,8 @@ u_int64_t getTimeMilliSecs()
 };
 
 u_int64_t getTimeMicroSecs()
-{ 
-  return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count(); 
+{
+  return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 }
 
 //High accuracy wait. Sacrifices some cpu cycles for accuracy
@@ -26,18 +26,17 @@ void spinWait(int microsecs, int sleepThresholdMilliseconds)
   u_int64_t baseTime = getTimeMicroSecs();
   u_int64_t goTime = baseTime + microsecs;
 
-  //sleep if we can 
-  int availableSleep = microsecs/1000 - sleepThresholdMilliseconds; 
-  if(availableSleep > 0 )
+  //sleep if we can
+  int availableSleep = microsecs / 1000 - sleepThresholdMilliseconds;
+  if (availableSleep > 0)
   {
-      std::this_thread::sleep_for (std::chrono::milliseconds(availableSleep));
+    std::this_thread::sleep_for(std::chrono::milliseconds(availableSleep));
   }
 
-  while(getTimeMicroSecs() < goTime)
+  while (getTimeMicroSecs() < goTime)
   {
-    //spin 
+    //spin
   };
-
 };
 
 void spinWait(int microsecs)
@@ -45,6 +44,73 @@ void spinWait(int microsecs)
   spinWait(microsecs, DEFUALT_SLEEP_THRESH_MS);
 }
 
+class Stopwatch
+{
+public:
+  Stopwatch();
+  int PRINT_UNITS;
+  void start();
+  u_int64_t stop();
+  u_int64_t stopAndPrintTime(std::string msg);
+  u_int64_t timeFunction(void (*f)(), std::string msg);
+  enum
+  {
+    TIME_UNIT_SECONDS,
+    TIME_UNIT_MILLISECONDS,
+    TIME_UNIT_MICROSECONDS
+  };
+
+private:
+  u_int64_t microTime;
+};
+
+Stopwatch::Stopwatch()
+{
+  this->PRINT_UNITS = Stopwatch::TIME_UNIT_MILLISECONDS;
+  this->microTime = 0;
 }
+
+void Stopwatch::start()
+{
+  this->microTime = getTimeMicroSecs();
+};
+
+u_int64_t Stopwatch::stop()
+{
+  return getTimeMicroSecs() - this->microTime;
+};
+
+u_int64_t Stopwatch::stopAndPrintTime(std::string msg)
+{
+  u_int64_t elapsedMicrosecinds = this->stop();
+  double time=0; 
+  std::string unitPrint = " microseconds"; 
+  switch (this->PRINT_UNITS)
+  {
+  case Stopwatch::TIME_UNIT_SECONDS:
+    unitPrint = " seconds";
+    time= ((double)elapsedMicrosecinds) / 1000000;
+    break;
+  case Stopwatch::TIME_UNIT_MILLISECONDS:
+    unitPrint = " milliseconds";
+    time= ((double)elapsedMicrosecinds) / 1000;
+    break;
+  default:
+    time=((double)elapsedMicrosecinds);
+    break;
+  }
+
+  std::cout << msg << " : " <<time << unitPrint<< std::endl;
+  return elapsedMicrosecinds;
+};
+
+u_int64_t Stopwatch::timeFunction(void (*f)(), std::string msg)
+{
+  this->start();
+  f();
+  return this->stopAndPrintTime(msg);
+}
+
+} // namespace extend
 
 #endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,24 @@
 #include "extend.h"
 
+void testFunc()
+{
+    extend::spinWait(1000);
+}
+
 void testSpinWait()
 {
     int nIters =200; 
-    int sleepTimeMicrosecs =20000; 
-    u_int64_t t0; 
+    int sleepTimeMicrosecs =20000;
+    extend::Stopwatch watch;  
+
+    watch.timeFunction(testFunc, "timed testFunc");
+
     double allTime =0.0; 
     for(int ii=0; ii<nIters; ii++)
     {
-        t0 = extend::getTimeMicroSecs(); 
+        watch.start();
         extend::spinWait(sleepTimeMicrosecs);
-        allTime += extend::getTimeMicroSecs() - t0; 
+        allTime += watch.stop(); 
     }
 
     double avg = (allTime/nIters) - sleepTimeMicrosecs; 
@@ -38,7 +46,7 @@ void testSHEM()
 
 int main()
 {
-    //testSpinWait();
-    testSHEM();
+    testSpinWait();
+    //testSHEM();
     return 0; 
 }
