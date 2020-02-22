22524eabd0bcd7ec824f2e216bf5038c037bad82
Ken  Ledford <kenledford@Kens-MBP.attlocal.net>
2020-02-22 02:50:52 -0800

    autoGit commit

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,17 +7,9 @@ rm -rf ./CMakeFiles
 rm -rf ./cmake_install.cmake
 rm -rf ./CmakeCache.txt
 
-rm -rf ./src/Makefile
-rm -rf ./src/CMakeFiles
-rm -rf ./src/cmake_install.cmake
-rm -rf ./src/CmakeCache.txt
-
 rm -rf ./test/Makefile
 rm -rf ./test/CMakeFiles
 rm -rf ./test/cmake_install.cmake
 rm -rf ./test/CmakeCache.txt
-
-rm -rf ./wrapCS/bin
-rm -rf ./wrapCS/obj
 
 rm *.log
